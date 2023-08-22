0ed7343d2499b6de3d3a15fb021e690a18104925
cristian-bosz <cristianbosz@gmail.com>
2023-08-22 14:45:39 -0300

    Registro de usuario

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ require_once('config/functions.php');
 
 <nav class="navbar navbar-expand-lg navbar-light bg-white">
       <div class="container">
-        <Link class="navbar-brand logo" to="/"><i class="bi bi-shop"></i> Bsz Store</Link>
+        <Link class="navbar-brand logo" to="/"><i class="bi bi-shop"></i> </Link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -36,7 +36,7 @@ require_once('config/functions.php');
              <?php                  
                     foreach ($navbar as $boton => $url):
             ?>
-                        <li class="nav-item">
+                        <li class="nav-item mx-4">
                         <a class="nav-link active" aria-current="page" href="index.php?seccion=<?= $url ?>"><?= $boton ?></a>
                         </li>
             <?php
@@ -44,19 +44,62 @@ require_once('config/functions.php');
             ?>
           </ul>
           <div class="d-flex align-items-center nav-Buttons">
-                  <Link class="text-decoration-none" to="/" >      
-                    <i class="text-dark bi bi-person-fill"></i>
-                  </Link>
-                  <Link class="text-decoration-none position-relative px-3" to="/" >   
-                    <i class="text-dark bi bi-cart-fill"></i>
-                    <span class="position-absolute top-0 start-105 translate-middle badge rounded-pill bg-success">
-                      7
-                    </span>
-                  </Link>
+          <li class="nav-item text-decoration-none dropdown">
+                
+            <?php
+						if (!isset($_SESSION['usuario'])):
+						?>
+							
+                  <a class="nav-link dropdown-toggle text-decoration-none" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="text-dark bi bi-person-fill"></i>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="index.php?seccion=login">Iniciar Sesión</a></li>
+                    <li><a class="dropdown-item" href="index.php?seccion=registro">Registrarme</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                  </ul>
+        
+						<?php
+						else:
+						?>
+
+
+                  <a class="nav-link dropdown-toggle text-decoration-none" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  		
+									<?php 
+                  if(empty($_SESSION['usuario']['img_user'])):
+                  ?>
+									<i class="text-dark bi bi-person-fill"></i>
+                  
+                  <?php
+                  elseif  (!empty($_SESSION['usuario']['img_user'])):
+                  ?>  
+                  <img src="img/users/<?=($_SESSION['usuario']['img_user'])?>" alt="Avatar de <?=($_SESSION['usuario']['nombre']) ?>" class="figure-img img-fluid  rounded-circle " style="height: 50px; width: 50px;"> 
+                  <?php
+                  endif;
+                  ?>
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="index.php?seccion=editar_perfil">Editar perfil</a></li>
+                    <li><a class="dropdown-item" href="features/logout.php">Cerrar sesión</a></li>
+                    <li><hr class="dropdown-divider"></li>
+                  </ul>
+
+
+
+						<?php
+						endif;
+						?>
+
+          </li>
           </div>
         </div>
       </div>
     </nav>
+
+
+
+
 
 
 
