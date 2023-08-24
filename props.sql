fd225b5adc8097b75f03f3d79c9e1ee7f5ffe303
cristian-bosz <cristianbosz@gmail.com>
2023-08-24 18:06:37 -0300

    Actualizacion de tablas en la bdd

--- props.sql
+++ props.sql
@@ -28,7 +28,107 @@ CREATE TABLE IF NOT EXISTS usuarios (
 
 INSERT INTO usuarios
 VALUES (1,'cristian', 'bösz', 'cristianbosz@hotmail.com', '2000/06/05', '$2y$10$50WxPV0HNkYLaQAnH4ccOuiBjmAjEVoxzJoIt6cNCCjpOK9GHMyPm', 'yo.png', 1);
-    
+
+
+CREATE TABLE IF NOT EXISTS baños (
+    baños_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    accesorios_baño VARCHAR(30) NOT NULL
+
+)ENGINE = InnoDB;
+
+INSERT INTO baños
+VALUES (1, 'Bañadera'),
+       (2, 'Agua caliente'),
+       (3, 'Secador de pelo'),
+       (4, 'Bidé'),
+       (5, 'Productos de limpieza'),
+       (6, 'Toallas'),
+       (7, 'Shampoo'),
+       (8, 'Jabón de cuerpo');
+
+
+CREATE TABLE IF NOT EXISTS habitaciones (
+    habitaciones_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    accesorios_habitaciones VARCHAR(30) NOT NULL
+
+)ENGINE = InnoDB;
+
+INSERT INTO habitaciones
+VALUES (1, 'Almohadas y mantas'),
+       (2, 'Perchas'),
+       (3, 'Vestidor o armario'),
+       (4, 'Mosquitero'),
+       (5, 'Plancha'),
+       (6, 'Persianas o cortinas'),
+       (7, 'Lavarropas'),
+       (8, 'Tender de ropa');
+
+
+CREATE TABLE IF NOT EXISTS cocina (
+    cocina_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    accesorios_cocina VARCHAR(30) NOT NULL
+
+)ENGINE = InnoDB;
+
+INSERT INTO cocina
+VALUES (1, 'Cocina'),
+       (2, 'Heladera'),
+       (3, 'Microondas'),
+       (4, 'Utensillos'),
+       (5, 'Vajilla y cubiertos'),
+       (6, 'Horno'),
+       (7, 'Tostadora'),
+       (8, 'Utensillos para parilla'),
+       (9, 'Mesa de comedor');
+
+
+CREATE TABLE IF NOT EXISTS exterior (
+    exterior_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    accesorios_exterior VARCHAR(30) NOT NULL
+
+)ENGINE = InnoDB;
+
+INSERT INTO exterior
+VALUES (1, 'Muebles'),
+       (2, 'Zona para comer'),
+       (3, 'Parrilla'),
+       (4, 'Zona para fogata'),
+       (5, 'Pileta');
+
+CREATE TABLE IF NOT EXISTS estacionamiento (
+    estacionamiento_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    accesorios_estacionamiento VARCHAR(30) NOT NULL
+
+)ENGINE = InnoDB;
+
+INSERT INTO estacionamiento
+VALUES (1, 'Estacionamiento o garage');
+
+
+CREATE TABLE IF NOT EXISTS entretenimiento (
+    entretenimiento_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    accesorios_entretenimiento VARCHAR(30) NOT NULL
+
+)ENGINE = InnoDB;
+
+INSERT INTO entretenimiento
+VALUES (1, 'Televisor'),
+        (2, 'Libros');
+
+
+CREATE TABLE IF NOT EXISTS calefaccion (
+    calefaccion_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    accesorios_calefaccion VARCHAR(30) NOT NULL
+
+)ENGINE = InnoDB;
+
+INSERT INTO calefaccion
+VALUES (1, 'Chimenea'),
+       (2, 'Ventiladores'),
+       (3, 'Estufas'),
+       (4, 'Aire acondicionado');
+
+
 CREATE TABLE IF NOT EXISTS comprar_prop (
     comprar_prop_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     titulo VARCHAR (120) NOT NULL,
@@ -43,24 +143,23 @@ CREATE TABLE IF NOT EXISTS comprar_prop (
     baños TINYINT NOT NULL,
     dormitorios TINYINT NOT NULL,
     cocheras TINYINT,
-    antiguedad TINYINT NOT NULL
+    antiguedad TINYINT NOT NULL,
+    baños_id_fk INT UNSIGNED NOT NULL,
+    habitaciones_id_fk INT UNSIGNED NOT NULL,
+    cocina_id_fk INT UNSIGNED NOT NULL,
+    exterior_id_fk INT UNSIGNED NOT NULL,
+    estacionamiento_id_fk INT UNSIGNED NOT NULL,
+    entretenimiento_id_fk INT UNSIGNED NOT NULL,
+    calefaccion_id_fk INT UNSIGNED NOT NULL,
+
+    FOREIGN KEY (baños_id_fk) REFERENCES baños(baños_id) ON DELETE CASCADE ON UPDATE CASCADE,   
+    FOREIGN KEY (habitaciones_id_fk) REFERENCES habitaciones(habitaciones_id) ON DELETE CASCADE ON UPDATE CASCADE,   
+    FOREIGN KEY (cocina_id_fk) REFERENCES cocina(cocina_id) ON DELETE CASCADE ON UPDATE CASCADE,   
+    FOREIGN KEY (exterior_id_fk) REFERENCES exterior(exterior_id) ON DELETE CASCADE ON UPDATE CASCADE,  
+    FOREIGN KEY (estacionamiento_id_fk) REFERENCES estacionamiento(estacionamiento_id) ON DELETE CASCADE ON UPDATE CASCADE, 
+    FOREIGN KEY (entretenimiento_id_fk) REFERENCES entretenimiento(entretenimiento_id) ON DELETE CASCADE ON UPDATE CASCADE, 
+    FOREIGN KEY (calefaccion_id_fk) REFERENCES calefaccion(calefaccion_id) ON DELETE CASCADE ON UPDATE CASCADE 
 )ENGINE = InnoDB;
 INSERT INTO comprar_prop
-VALUES (1,'mansion', '', '', 'Terrible mansion viral papaa', 190000, 500, 20, 25, 4, 3, 4, 1, 50);
-    
-
-CREATE TABLE IF NOT EXISTS baños (
-    baños_id INT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    bañera BOOLEAN,
-    agua_caliente BOOLEAN,
-    secador_pelo BOOLEAN,
-    bidé BOOLEAN,
-    productos_limpieza BOOLEAN,
-    comprar_prop_id_fk INT UNSIGNED NOT NULL,
-
-    FOREIGN KEY (comprar_prop_id_fk) REFERENCES comprar_prop(comprar_prop_id) ON DELETE RESTRICT ON UPDATE CASCADE
-)ENGINE = InnoDB;
-
-INSERT INTO baños
-VALUES (1, true, true, false, true, false, 1);
+VALUES (1,'mansion', '', '', 'Terrible mansion viral papaa', 190000, 500, 20, 25, 4, 3, 4, 1, 50, 1,1,1,1,1,1,1);
     
